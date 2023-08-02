6bedaa2c158714902a822364acca05e2b954f065
Lukáš Matuška <matuska.lukas@lukasmatuska.cz>
2023-08-02 10:26:41 +0200

    Fix CORS

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 from flask import Flask
+from flask_cors import CORS
 import datetime
 import os
 
 app = Flask(__name__)
 
+CORS(app)
 
 def get_data_from_url(url):
     response = requests.get(url)
